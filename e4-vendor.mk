d128b0d5a4e127b1ba2b8e364664cb80098035d6
darklord4822 <darklord4722@gmail.com>
2017-10-07 12:43:21 +0500

    first commit

---
+++ e4-vendor.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/thh,system/vendor/thh)
